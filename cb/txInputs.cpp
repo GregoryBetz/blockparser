2d67162c70895de23549cc5d2355f7f6649be058
Mariusz Nowostawski <nowostawski@gmail.com>
2015-12-08 13:33:30 +0100

    Adding volume and tx hash.

--- cb/txInputs.cpp
+++ cb/txInputs.cpp
@@ -12,13 +12,17 @@ struct TxInputs : public Callback {
 
     
     // global Tx with maximum nb of inputs
+    uint8_t txHash[256];
+
     uint128_t maxInputCountInTx; 
     uint128_t nbOutputs; 
-
+    uint128_t volume;
 
     // temporary counters for current Tx
+    uint8_t tmpTxHash[256];
     uint128_t tmpMaxInputCountInTx;     
-    uint128_t tmpOutputs; 
+    uint128_t tmpOutputs;
+    uint128_t tmpVolume; 
     
 
     TxInputs() {
@@ -45,6 +49,7 @@ struct TxInputs : public Callback {
         int argc,
         const char *argv[]
     ) {
+        volume = 0;
         nbOutputs = 0;
         maxInputCountInTx = 0;
 
@@ -62,36 +67,45 @@ struct TxInputs : public Callback {
         const uint8_t *outputScript,
         uint64_t      outputScriptSize
     ) {
-        // volume += value;
+        tmpVolume += value;
     }
 
     virtual void wrapup() {
         printf("\n");
         #define P(x) (pr128(x).c_str())
 
+            printf("    The TX found: ");
+            showHex(txHash);
             printf("    maxInputCountInTx = %s\n", P(maxInputCountInTx));
             printf("    nbOutputs = %s\n", P(nbOutputs));
+            printf("    volume = %s\n", P(volume));
             printf("\n");
 
         #undef P
     }
 
-    virtual void         startTX(const uint8_t *p, const uint8_t *hash) 
+    virtual void startTX(const uint8_t *p, const uint8_t *hash) 
     { 
             if (tmpMaxInputCountInTx > maxInputCountInTx) 
             {
                 maxInputCountInTx = tmpMaxInputCountInTx;
                 nbOutputs = tmpOutputs;
+                volume = tmpVolume;
+                memcpy(txHash, tmpTxHash, 256);
 
             }
+            memcpy(tmpTxHash, hash, 256);
             tmpMaxInputCountInTx = 0; 
             tmpOutputs = 0;
+            tmpVolume = 0;
     }
-    virtual void      startInput(const uint8_t *p                     ) 
+
+    virtual void startInput(const uint8_t *p) 
     { 
         ++tmpMaxInputCountInTx; 
     }
-    virtual void     startOutput(const uint8_t *p                     ) 
+
+    virtual void startOutput(const uint8_t *p) 
     { 
         ++tmpOutputs;     
     }
